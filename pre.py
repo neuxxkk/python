3fee7c09f25a5bb1470681755bddeed6abcaa4bf
neuxxkk <vitornms@gmail.com>
2021-11-06 02:47:42 -0300

    a

---
+++ pre.py
@@ -0,0 +1,11 @@
+from os import system
+from sys import platform
+
+if platform[0]=='l' or platform[0]=='d':
+    def cls():
+        system('clear')
+elif platform[0]=='w':
+    def cls():
+        system('cls')
+        
+cls()
