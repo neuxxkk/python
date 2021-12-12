b4d6a41a8dfa7776cb1be069fd9440a0d9cd960f
neuxxkk <vitornms@gmail.com>
2021-12-11 21:47:58 -0300

    a

--- exercise/statements/ex040.py
+++ exercise/statements/ex040.py
@@ -3,10 +3,14 @@ cls()
 
 t=0
 q=int(input('how many grades: '))
-
+x=int(input('max grade: '))
+r=x*q
 for i in range(q):
     n=int(input(f'nota {i+1}: '))
     t=t+n
 
+
 m=t/q
-print(m)
+
+if m>=r*0.60:print(f'{m}/{r} = good')
+else:print(f'{m}/{r} = bad')
