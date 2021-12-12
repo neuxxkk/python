6c90d7746951fee178cc0c0de6ef718674575a78
neuxxkk <83613773+neuxxkk@users.noreply.github.com>
2021-12-12 01:00:35 -0300

    update ex044.py

---
+++ exercise/statements/ex044.py
@@ -0,0 +1,13 @@
+from pre import cls
+cls()
+
+
+n=int(input('value: '))
+
+print('1- a vista\n2- a vista cartao\n3- 2x cartao\n4- 3x cartao')
+c=int(input())
+
+if c==1:n=n-(n*0.10)  
+
+
+print(n)
