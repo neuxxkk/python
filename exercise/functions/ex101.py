28bc0b33c60ae8b58e75c735eb883b38e8acd99b
neuxxkk <vitornms@gmail.com>
2021-12-27 23:29:24 -0300

    a

--- exercise/functions/ex101.py
+++ exercise/functions/ex101.py
@@ -1,3 +1,12 @@
+from datetime import datetime
 from pre import cls
 cls()
 
+def f(x):
+    a=datetime.now().year-x
+    if a<16:c='PROIBIDO' 
+    elif a<18 or a>60:c='OPCIONAL'
+    else: c='OBRIGATÓRIO'
+    return(f'Voto com {a} anos = {c}')
+
+print(f(int(input('Data de nascimento: '))))
