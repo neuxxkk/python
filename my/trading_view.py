a7b45c22a76e2cb284eb1788caf5ac6069ad7150
= <=>
2023-01-27 03:25:21 -0300

    a

--- my/trading_view.py
+++ my/trading_view.py
@@ -1,5 +1,5 @@
-from python.pre import *
-Clean.cls()
+from pre import cls
+cls()
 import requests as req
 from bs4 import BeautifulSoup 
 import matplotlib.pyplot as plt
@@ -8,7 +8,7 @@ from tkinter import PhotoImage
 
 soup = BeautifulSoup #Instanciando p/ ficar + fácil
 
-day = date().tm_mday#                                            |
+day = date().tm_mday#                                       |
 mon = date().tm_mon#  definindo como str para entar na URL  |
 year = date().tm_year#                                      V
 
@@ -28,7 +28,7 @@ def graphic(period, coin):
   plt.subplots_adjust(left = 0.084, right = 0.95, bottom = 0.116, top = 0.895)
   plt.title(f'{coin}-BRL', font = 'candara', color = '#2962FF', fontsize = 15, fontweight = 'bold', loc='left', pad=17)
   plt.xlabel('Days', color = '#D1D4DC', labelpad=15, fontsize = 11)
-  plt.ylabel('1BTC price', color = '#D1D4DC', labelpad=15, fontsize = 11)
+  plt.ylabel(f'1{coin} price', color = '#D1D4DC', labelpad=15, fontsize = 11)
 
 #                            0 = início;  x = final(até);  -1 = steps(regressivo) 
   for i in range(period, 0, -1):
@@ -48,11 +48,12 @@ def graphic(period, coin):
   btc = float(btc.bid.text)
   plt_values.append(btc)
   plt_days.append(day)
+  txt = f'{btc:.2f}'
 
-  t = plt.text(day-0.175, btc+0.05, str(btc), color = 'white', font = 'calibri', size = 11, fontstyle = 'normal', fontweight = 'bold')
+  t = plt.text(day-(day*0.0005), btc+(btc*0.0005), txt, color = 'white', font = 'calibri', size = 11, fontstyle = 'normal', fontweight = 'bold')
   t.set_bbox(dict(facecolor = '#2962ff', boxstyle = 'round'))
   plt.plot(plt_days, plt_values, color = '#2962ff')
   plt.show()
 
 
-graphic(3,'BTC')
+graphic(3,'EUR')
